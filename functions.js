6cca19c697a6e5baf1861bdac010c40b3cb5f815
Nathan Presley <npresley@strong-hold.com>
2021-08-13 09:37:28 -0400

    need to work through event listeners for input

--- functions.js
+++ functions.js
@@ -2,22 +2,31 @@ let startingTeams = [];
 let teams = document.getElementsByClassName("team");
 let teamRank = document.getElementsByClassName("team-rank");
 let inputName = document.getElementsByClassName("team-name");
-const inputScore = document.getElementsByClassName("team-score");
+let inputScore = document.getElementsByClassName("team-score");
 
 const addTeams = () => {
     let team = {
-        rank: "",
-        teamName: "",
-        score: "",
+        rank: teamRank,
+        teamName: inputName.value,
+        score: inputScore.value
     }
+    startingTeams.push(team);
     console.log(startingTeams);
+}
+
+//***issue*** when you click off the input element, it is no longer the active element.
+function removeInputField() {
+    let currentInputField = document.activeElement;
+    currentInputField.remove();
 }
 
 Array.from(inputScore).forEach(function(inputScore) {
     inputScore.addEventListener('focusout', function(){addTeams()});
 });
 
-
+Array.from(inputName).forEach(function(inputName) {
+    inputName.addEventListener('onblur', function(){removeInputField()});
+});
 
 
 // Advances winning team to next bracket 
