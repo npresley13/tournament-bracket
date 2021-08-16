204557332d6f1e5df0ee4519fd4fb265fd840554
Nathan Presley <npresley13@gmail.com>
2021-08-15 21:01:12 -0400

    add team inputs to array startingTeams

--- functions.js
+++ functions.js
@@ -4,35 +4,15 @@ let teamRank = document.getElementsByClassName("team-rank");
 let inputName = document.getElementsByClassName("team-name");
 let inputScore = document.getElementsByClassName("team-score");
 
-const addTeams = () => {
-    let team = {
-        rank: teamRank,
-        teamName: inputName.value,
-        score: inputScore.value
-    }
-    startingTeams.push(team);
-    console.log(startingTeams);
-}
+//     //Save to Local Storage
+//     localStorage.setItem('MyTeams', JSON.stringify(startingTeams));
+// }
 
 //***issue*** when you click off the input element, it is no longer the active element.
-function removeInputField() {
-    let inputNameArr = Array.from(inputName);
-    for (let i = 0; i < inputNameArr.length; i++) {
-        let teamID = "team"+(i+1);
-        let currentTeamID = document.getElementById(teamID);
-        if (currenTeamID.value !== "") {
-            currentTeamID.remove();
-        }
-    }
-    
-}
+
 
 Array.from(inputScore).forEach(function(inputScore) {
-    inputScore.addEventListener('focusout', function(){addTeams()});
-});
-
-Array.from(inputName).forEach(function(inputName) {
-    inputName.addEventListener('focusout', function(){removeInputField()});
+    inputScore.addEventListener('focusout', function(){buildTeams(20)});
 });
 
 
@@ -45,3 +25,14 @@ Array.from(inputName).forEach(function(inputName) {
 //     }
 // }
 
+//builds an array from input values 
+function buildTeams(numTeams) {
+    for(i = 0; i < numTeams; i++) {
+        let team = {
+            rank: document.getElementById("rank"+(i+1)),
+            teamName: document.getElementById("team"+(i+1)).value,
+        }
+        startingTeams.push(team);
+    }
+    console.log(startingTeams);
+}
