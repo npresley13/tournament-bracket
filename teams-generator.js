7835b4eb12c32d4819a3031de101a7a2e1eb6505
Nathan Presley <npresley@strong-hold.com>
2021-08-25 08:05:26 -0400

    added preventDefault to function to allow it to gather data

--- teams-generator.js
+++ teams-generator.js
@@ -2,16 +2,19 @@
 let teamSubmit = document.getElementById("num-teams-submit");
 let teamList = document.getElementById("team-input-list");
 
-teamSubmit.addEventListener("submit",buildTeams());
+teamSubmit.addEventListener("click",function(e) {
+    buildTeams(e)
+});
 
-function buildTeams() {
+function buildTeams(e) {
+    e.preventDefault();
     let numTeams = document.getElementById("num-teams").value;
-    console.log(numTeams);
-    // for(let i = 0; i < numTeams; i++) {
-    //     let newInput = document.createElement("input");
-    //     newInput.type = "text";
-    //     newInput.placeholder = `Team #${i+1}`;
-    //     newInput.className = "teamName";
-    //     teamList.appendChild(newInput);
-    // }
+    teamList.innerHTML = "";
+    for(let i = 0; i < numTeams; i++) {
+        let newInput = document.createElement("input");
+        newInput.type = "text";
+        newInput.placeholder = `Team #${i+1}`;
+        newInput.className = "teamName";
+        teamList.appendChild(newInput);
+    }
 }
