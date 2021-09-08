24313aa3f771d9adc5efc4301070a6d0c7202680
Nathan Presley <npresley@strong-hold.com>
2021-09-08 09:52:02 -0400

    removed comments

--- functions.js
+++ functions.js
@@ -7,33 +7,6 @@ let inputScore = document.getElementsByClassName("team-score");
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.team');
 
-// Adds event listener to when someone clicks out of the score box
-// this event listner will be used to advance teams to the next round.
-// Array.from(inputScore).forEach(function(inputScore) {
-//     inputScore.addEventListener('focusout', function(){buildTeams(20)});
-// });
-
-
-// Advances winning team to next bracket 
-// let scoreCheck = function (teamOnescore, teamTwoscore) {
-//     if (teamOnescore > teamTwoscore) {
-//         return teamOne;
-//     } else {
-//         return teamTwo;
-//     }
-// }
-
-
-// Match teams saved to the Array with the proper bracket.
-// Creates an array from local storage then places teams based on the Bracket slot ID
-// function placeTeams() {
-//     let teamArr2 = JSON.parse(window.localStorage.getItem('team'));
-//     for (let i = 0; i < teamArr2.length; i++) {
-//         let nameTeam = document.getElementById(`team${i+1}`);
-//         nameTeam.innerHTML = teamArr2[i].teamName;
-//     }
-// }
-
 
 function placeTeams() {
     let teamArr2 = JSON.parse(window.localStorage.getItem('team'));
@@ -49,11 +22,12 @@ function placeTeams() {
     }
 }
 
+
 // Builds the bracket on page load
 placeTeams();
 
-// DRAG AND DROP 2
 
+// DRAG AND DROP
 let draggable;
 
 document.addEventListener('dragstart', function(event) {
