3f788f8107e1e617e8932d48570e2df7ca919e98
Nathan Presley <npresley@strong-hold.com>
2021-11-12 15:43:49 -0500

    timer color

--- js/counter.js
+++ js/counter.js
@@ -2,10 +2,17 @@ const deadline = '12/1/2021';
 
 function getTimeRemaining(endTime) {
     const total = Date.parse(endTime) - Date.parse(new Date());
-    const days = Math.floor( total/(1000*60*60*24) );
+    const days = Math.floor( total/(1000*60*60*24) ) + 1;
     return days;
 }
 
 let timeRemaining = getTimeRemaining(deadline);
 
+function turnRed(time) {
+    if(time <= 5) {
+        document.getElementById('counter').style.color = "red";
+    }
+}
+
 document.getElementById('counter').innerHTML = timeRemaining + " days";
+turnRed(timeRemaining);
