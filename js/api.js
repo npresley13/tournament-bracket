505721ecd13ce7bb1eb2b045b745df0d8a8c094e
Nathan Presley <npresley13@gmail.com>
2021-11-18 15:47:50 -0500

    added boxes around the temp and countdown

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-// api.openweathermap.org/data/2.5/weather?q=Louisville&appid=d84b7e6198ac6de0ecb11c2a2eb3c7d3
+// API used to pull temperature
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Louisville&appid=d84b7e6198ac6de0ecb11c2a2eb3c7d3")
     .then(function (response) {
@@ -6,7 +6,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Louisville&appid=d84b7e
     })
     .then(function (data) {
         console.log(data);
-        let temp = document.createElement('p');
+        let temp = document.createElement('div');
         temp.setAttribute("id", "temperature");
         temp.innerHTML =  `${Math.round(((data.main.temp_max - 273.15) * 1.8) + 32)}&#8457`;
         document.getElementById('weather').appendChild(temp);
